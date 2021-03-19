686fd54280acea168422ea0e0728915f58d6b56a
isabellarl573 <exoplaneta573@DESKTOP-3KANHKI.localdomain>
2021-03-18 22:26:30 -0400

    Part 2 demo

---
+++ simulator1/part2_demo.cpp
@@ -0,0 +1,46 @@
+nclude <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+
+using namespace std;
+
+int allocate_process (int size, int PID) {
+        return 0;
+}
+
+int deallocate_process (int PID) {
+        //merge buddy blocks?
+        //    return 0;
+        //    }
+        //
+        //    int main()
+        //    {
+        //        /* initialize random seed: */
+        //            srand (time(NULL));
+        //                //get time
+        //                    //hash map -> memory
+        //                        //first element is PID, secong how much space is used of that size?
+        //
+        //                            //memory -> how many pages (2^n)
+        //
+        //                                //hash table with binary tree as the elements?
+        //                                    //buddy blocks initial 64 pages?
+        //
+        //
+        //                                        //if size is larger than block size, separate process
+        //                                            //buddy blocks?
+        //                                                int PID = 0;
+        //                                                    for (int n = 0; n < 10; n++) {
+        //                                                            numProcesses = rand() % 100;
+        //
+        //                                                                    for (int i = 0; i < numProcesses; i++) {
+        //
+        //                                                                            }
+        //                                                                                }
+        //                                                                                    cout << "Hello world!" << endl;
+        //                                                                                        return 0;
+        //
+        //                                                                                            //request at t=1738 for i=33, size(i)=12 pages,
+        //                                                                                                //satisfied by free block of size 32 starting at 8056 leaving 20 pages as 1x16 + 2x2
+        //                                                                                                }
+        //
